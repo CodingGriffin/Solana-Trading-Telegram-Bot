f5180e9afbeff82e644d93aa5e3c0e2c4a0a8113
dynaput247 <broweqos302679@hotmail.com>
2024-06-30 18:12:09 +0900

    edit telegram bot

--- src/telegram-bot-api.d.ts
+++ src/telegram-bot-api.d.ts
@@ -1,1 +1,8 @@
 declare module 'telegram-bot-api';
+
+// Implement telegram-related functions here
+// Respond to the callback query with an alert and update the bot's message
+//   bot.answerCallbackQuery(callbackQuery.id, {
+//     text: `You pressed ${category}`,
+//     show_alert: true,
+//   });
